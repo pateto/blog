5b28129e904a396735c19fd2ddc3c7dafbe68862
pateto <alvaroecaceres7@gmail.com>
2023-06-21 11:35:38 -0500

    add requirements

--- users/models.py
+++ users/models.py
@@ -2,4 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+
+    # set email as username for login
+    #USERNAME_FIELD = 'email'
+    #REQUIRED_FIELDS = []
